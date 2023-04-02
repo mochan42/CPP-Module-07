9696d5e83208a05d1b9c35a337963d5e72b40156
Monine Chan <mochan@students.42wolfsburg.de>
2023-04-02 19:33:09 +0200

    add tests from eval sheet

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,27 +6,58 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 08:49:11 by moninechan        #+#    #+#             */
-/*   Updated: 2023/04/02 17:50:20 by mochan           ###   ########.fr       */
+/*   Updated: 2023/04/02 19:32:33 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MathOperators.hpp"
 
+class Awesome
+{
+	public:
+		Awesome(void) : _n(0) {}
+		Awesome( int n ) : _n( n ) {}
+		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+		bool operator<=( Awesome const & rhs ) const {return (this->_n <= rhs._n); }
+		int get_n() const { return _n; }
+
+	private:
+		int	_n;
+};
+
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
 int main(int, char**)
 {
-	int	a = 3;
-	int	b = 2;
+	{
+		std::cout << CY << "========= Main Tests =========" << D << "\n";
+		int	a = 3;
+		int	b = 2;
 
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
 
+	{
+		std::cout << PU << "\n====== Eval Sheet tests ======" << D << "\n";
+		Awesome a(2), b(4);
+		swap(a,b);
+		std::cout << a << " " << b << std::endl;
+		std::cout << max(a, b) << std::endl;
+		std::cout << min(a, b) << std::endl;
+	}
 	return 0;
 }
