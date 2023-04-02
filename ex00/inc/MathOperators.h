57ea368d1f09e3942ebd0cf7ef5832d98fc7761a
mochan42 <mochan@students.42wolfsburg.de>
2023-04-02 08:59:06 +0200

    setup ex00

---
+++ ex00/inc/MathOperators.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MathOperators.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/02 08:55:03 by moninechan        #+#    #+#             */
+/*   Updated: 2023/04/02 08:56:21 by moninechan       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MATHOPERATORS_H
+#define MATHOPERATORS_H
+
+#include "MathOperators.hpp"
+
+// *** COLORS ***//
+# define BLANK "\e[0m"
+# define BLA "\033[0;30m"
+# define RED "\033[0;31m"
+# define BDRED "\033[1;31m"
+# define GREEN "\033[0;32m"
+# define BDGRN "\033[1;32m"
+# define LIGRN "\033[38;5;82m"
+# define BKLIGRN "\033[30;48;5;82m"
+# define DKGRN "\033[38;5;22m"
+# define YELL "\033[0;33m"
+# define BLU "\033[0;34m"
+# define PU "\033[0;35m"
+# define CY "\033[0;36m"
+# define WH "\033[0;37m"
+# define D "\033[0m"
+
+#endif
---
+++ ex00/inc/MathOperators.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MathOperators.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/02 08:55:06 by moninechan        #+#    #+#             */
+/*   Updated: 2023/04/02 08:57:06 by moninechan       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MATHOPERATORS_HPP
+#define MATHOPERATORS_HPP
+#include <iostream>
+
+
+#endif
